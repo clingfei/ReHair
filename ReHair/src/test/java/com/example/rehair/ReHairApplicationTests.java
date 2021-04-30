0381789694d216733e705fef308a8bbf1de1036f
clingfei <1599101385@qq.com>
2021-05-01 01:11:10 +0800

    add configure files

---
+++ ReHair/src/test/java/com/example/rehair/ReHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rehair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReHairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
