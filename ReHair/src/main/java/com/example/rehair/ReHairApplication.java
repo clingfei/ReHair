4312e019f50daa31d005a410074eac793b3721ef
clingfei <1599101385@qq.com>
2021-05-01 18:58:07 +0800

    enable https

--- ReHair/src/main/java/com/example/rehair/ReHairApplication.java
+++ ReHair/src/main/java/com/example/rehair/ReHairApplication.java
@@ -1,11 +1,15 @@
 package com.example.rehair;
 
-import org.slf4j.LoggerFactory;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.logging.Logger;
 
 @SpringBootApplication
 @ComponentScan (basePackages = {"com.example.rehair.controller", "com.example.rehair.dao", "com.example.rehair.model", "com.example.rehair.service", "com.example.rehair.service.impl"})
@@ -16,4 +20,32 @@ public class ReHairApplication {
         SpringApplication.run(ReHairApplication.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(8080);
+        connector.setSecure(true);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(8081);
+        return connector;
+    }
 }
