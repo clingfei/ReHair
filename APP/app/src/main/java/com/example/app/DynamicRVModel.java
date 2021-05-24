891ec87af1150612b58c4b351d1c81c16ac7bed2
Ninoric <ninomiyakazunari@sjtu.edu.cn>
2021-05-24 17:46:09 +0800

    moments main page demo

---
+++ APP/app/src/main/java/com/example/app/DynamicRVModel.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+public class DynamicRVModel {
+    String name;
+
+    public DynamicRVModel(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
