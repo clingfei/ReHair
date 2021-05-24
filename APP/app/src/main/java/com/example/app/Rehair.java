9412150926c367fa8d5c820a4f9e70160ba41140
Bizzle <bizzle@sjtu.edu.cn>
2021-05-24 12:33:26 +0800

    adjust camera function

--- APP/app/src/main/java/com/example/app/Rehair.java
+++ APP/app/src/main/java/com/example/app/Rehair.java
@@ -2,14 +2,25 @@ package com.example.app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-
+import android.widget.ImageView;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class Rehair extends AppCompatActivity {
+
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private ImageView picture;
+    private Intent intent1, intent2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +49,48 @@ public class Rehair extends AppCompatActivity {
                 return false;
             }
         });
+
+        Button chooseFromAlbum = findViewById(R.id.choose_from_album);
+        chooseFromAlbum.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(Rehair.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(Rehair.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CHOOSE_PHOTO);
+                } else {
+                    openAlbum(); //打开album界面
+                }
+            }
+        });
+        Button takePhoto = findViewById(R.id.take_photo);
+        picture = findViewById(R.id.picture);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //动态申请权限
+                if (ContextCompat.checkSelfPermission(Rehair.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Rehair.this, new String[]{Manifest.permission.CAMERA}, TAKE_PHOTO);
+                } else {
+                    //启动相机程序
+                    startCamera();
+                }
+            }
+        });
+        intent1=new Intent(this,Albums.class);//创建跳转到Albums显示的窗口的Intent
+        intent2=new Intent(this,Camera.class);//创建跳转到Camera显示的窗口的Intent
     }
+
+    private void openAlbum() {
+        startActivity(intent1);//进入album的窗口界面
+    }
+    private void startCamera() {
+        startActivity(intent2);//进入camera的窗口界面
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        // 指定图片的输出地址为imageUri
+        //mPhotoPath = getSDPath()+"/"+ getPhotoFileName();//设置图片文件路径，getSDPath()和getPhotoFileName()具体实现在下面
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        //startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+
 }
