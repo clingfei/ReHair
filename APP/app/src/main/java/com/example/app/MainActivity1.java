36368222281bdea9db6e7e61c6170216a42dc310
jackyu0001 <1485047041@qq.com>
2021-05-27 21:23:16 +0800

    adjust

--- APP/app/src/main/java/com/example/app/MainActivity1.java
+++ APP/app/src/main/java/com/example/app/MainActivity1.java
@@ -76,11 +76,11 @@ public class MainActivity1 extends AppCompatActivity {
 
     ImageButton.OnClickListener listener = new Button.OnClickListener() {
         public void onClick(View v) {
-<<<<<<< .merge_file_a06020
-            Intent intent = new Intent(MainActivity1.this, Account.class);
-=======
-            Intent intent = new Intent(MainActivity1.this, Moments.class);
->>>>>>> .merge_file_a21004
+
+            Intent intent= new Intent(MainActivity1.this, Account.class);
+
+            Intent intent1 = new Intent(MainActivity1.this, Moments.class);
+
             startActivity(intent);
             MainActivity1.this.finish();
         }
